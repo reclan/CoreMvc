06f93994007f15b5226b90d7c006e6a1c813cb84
Constantine Kolobkov <reclan@hotmail.com>
2020-07-12 23:48:06 +0300

    feat: add logging posibility

--- CoreMvc/Controllers/HomeController.cs
+++ CoreMvc/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace CoreMvc.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
